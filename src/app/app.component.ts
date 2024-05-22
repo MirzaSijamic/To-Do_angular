fc37f7892188fb691fd0d09feaeee0f408f3880d
unknown <mirza.sijamic04@gmail.com>
2024-05-22 21:36:06 +0200

    ???

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { TasksComponent } from './components/tasks/tasks.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, TasksComponent, FontAwesomeModule, HttpClientModule]
+})
+export class AppComponent {
+  title = 'Task Tracker';
+}
